5223ded2834fae540b93208522b6a90f0bc1631a
Ayush <ayush1911070@akgec.ac.in>
2020-09-13 11:44:15 +0530

    first commit

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            color: Colors.indigoAccent,
+            child: Column(
+              children: <Widget>[
+                FoodLogo(),
+                SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                  children: <Widget>[
+                    style("DRINK    ", 20.0),
+                    style("PIZZA    ", 20.0),
+                    style("BURGER    ", 20.0),
+                    style("SANDWICH    ", 20.0),
+                    style("ICE CREAM    ", 20.0),
+                    style("CAKE    ", 20.0),
+                  ],
+                )),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+
+  Widget style(String text, double size) {
+    return Text(
+      text,
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+          decoration: TextDecoration.none,
+          fontSize: size,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.w400,
+          color: Colors.white),
+    );
+  }
+}
+
+class FoodLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/foodlogo.jpg');
+    Image image = Image(
+      image: assetImage,
+      width: 150.0,
+      height: 150.0,
+    );
+    return Container(
+      child: image,
+    );
+  }
+}
